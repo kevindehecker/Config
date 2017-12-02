f0e8f0845a13e26b3a0591581324a196d935326d
kevindehecker <kevindehecker@hotmail.com>
2017-12-02 19:35:18 +0100

    Update mount_share.sh

--- scripts/mount_share.sh
+++ scripts/mount_share.sh
@@ -2,3 +2,5 @@ id -g ubuntu
 id -u ubuntu
 
 sudo mount -t cifs //192.168.1.11/jetserketser -o username=houjebek,password=<PASSWORD>,uid=1000,gid=1000 hv
+
+sudo mount -t cifs -o vers=1.0 //192.168.1.11/jetserketser -o username=houjebek,password=pass,uid=1000,gid=1000 hv
