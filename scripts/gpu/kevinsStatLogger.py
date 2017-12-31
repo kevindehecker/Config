41d54b7a8985bba6631b35dacffc4f15d2e341a8
Kevin <kevindehecker@hotmail.com>
2017-12-31 16:53:15 +0100

    Logging

--- scripts/gpu/kevinsStatLogger.py
+++ scripts/gpu/kevinsStatLogger.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python2
 
+#this daemon script logs usage statistics and starts a coin miner if gpu's are not used
+#made by Kevin
+
 from __future__ import print_function
 import psutil
 import time
@@ -9,19 +12,22 @@ import sys
 import signal
 import subprocess
 
-PROCNAME = "/home/kevin/ccminer/ccminer -a x17 -o stratum+tcp://yiimp.eu:3737 -u D9uarTgV9p7cywXfv8eMjnxtqxbCMMDbrq -d"
-PROCNAME_SHORT = "/home/kevin/ccminer/ccminer"
-INTERVAL = 60
+MINER_PROC_NAME = "/home/kevin/ccminer/ccminer -a x17 -o stratum+tcp://yiimp.eu:3737 -u D9uarTgV9p7cywXfv8eMjnxtqxbCMMDbrq -d"
+MINER_PROC_NAME_SHORT = "/home/kevin/ccminer/ccminer"
+INTERVAL = 10
 NGPUS = 2
 
 
 users = []
+gpu_users = [0] * NGPUS
 
 mining_pids = [psutil.Process] * NGPUS
 mining_started = [False] * NGPUS
 
 while True:
-	proc = subprocess.Popen(['nvidia-smi'],stdout=subprocess.PIPE) #TODO: change to nvidia-smi util
+	time_str =time.strftime("%I:%M:%S") + ' ' + time.strftime("%d/%m/%Y")
+	#proc = subprocess.Popen(['nvidia-smi'],stdout=subprocess.PIPE) 
+	proc = subprocess.Popen(['python', 'fake_utility.py'],stdout=subprocess.PIPE) #TMP
 
 	state = 0
 	gpu_mem_usages = [0] * NGPUS
@@ -30,10 +36,20 @@ while True:
 
 	for i in range (0,NGPUS):
 		gpu_mem_usages[i] = []
+		gpu_users[i] = []
 
+	gpu_overview_id = 0
 	while True:
 		line = proc.stdout.readline()
-		if line != '':    	    
+		if line != '':
+
+			if '%' in line:
+				#log this line to appropiate file, contains overall GPU usage statistics
+				with open('gpu' + str(gpu_overview_id) + '.txt', 'a') as f:
+					f.write(time_str + ' ' + line )
+				gpu_overview_id = gpu_overview_id +1
+
+
 			if line.startswith("+-----"): #start and end of the output, reset state
 				state=0
 			if state==0:
@@ -47,42 +63,53 @@ while True:
 				
 				gpu_id = int(ls[1])
 				process_id = int(ls[2])
-				p = psutil.Process(process_id)
+				p = psutil.Process(832)
 				process_type = ls[3]
 				process_name = ls[4]
 				process_user = p.username()
-				process_cpu = p.cpu_percent()
+				process_cpu = p.cpu_percent(interval=0.01)
 				mem_text = ls[5]
-				process_mem = mem_text[0:len(mem_text)-3] #currently assuming it is always in MiB
-				print (gpu_id,": ", process_name,process_user,process_cpu, process_mem)
+				process_mem = mem_text[0:len(mem_text)-3] #currently assuming this is always in MiB
+#				print (gpu_id,": ", process_name,process_user,process_cpu, process_mem)
 				
 				if process_user not in users:
 					users.append(process_user)
+				if process_user not in gpu_users[gpu_id]:
+					gpu_users[gpu_id].append(process_user)
 
-				if process_name != "/usr/lib/xorg/Xorg" and process_name != PROCNAME_SHORT:
+				if process_name != "/usr/lib/xorg/Xorg" and process_name != MINER_PROC_NAME_SHORT:
 					gpu_is_used[gpu_id] = True
-				if process_name == PROCNAME_SHORT:
+				if process_name == MINER_PROC_NAME_SHORT:
 					gpu_is_mining[gpu_id] = True
 		else: #end of smi output
 			break
 
-	print("used: ", gpu_is_used)
-	print("mining: ", gpu_is_mining)
-	print(users)
+	print("GPU used:   ", gpu_is_used)
+	print("GPU mining: ", gpu_is_mining)
+	print("GPU users: ",  users)
+
+	with open('general.txt', 'a') as f:
+		f.write(time_str + ';' + str(gpu_is_used) + ';' + str(gpu_is_mining) + ';' + str(gpu_users) + '\n')
 
 	for i in range(0,NGPUS):
 		if not gpu_is_used[i]and not gpu_is_mining[i]:
 			#start miner on this gpu
-			print("Starting miner on GPU", i)
-			pro = Popen(PROCNAME + str(i),shell=True,preexec_fn=os.setsid)
+			print(time_str + ": starting miner on GPU", i)
+			pro = Popen(MINER_PROC_NAME + str(i),shell=True,preexec_fn=os.setsid)
 			mining_pids[i] = pro
 			mining_started[i] = True
 		elif gpu_is_used[i] and gpu_is_mining[i]:
 			#stop miner on this gpu
-			print("Stopping miner on GPU", i)
+			print(time_str + ": stopping miner on GPU", i)
 			os.killpg(os.getpgid(mining_pids[i].pid), signal.SIGTERM)
 			mining_started[i] = False
 		else:
-			print("Nothing to do for GPU", i)
+			print(time_str + ": nothing to do for GPU", i)
 
-	time.sleep(INTERVAL)
+	#log CPU usage:
+	mem =psutil.virtual_memory()
+	cpu = str(psutil.cpu_percent(INTERVAL))
+	with open('cpu.txt', 'a') as f:
+		f.write(time_str + ': ' + cpu + ', ' + str(mem.percent) + ', ' + str(mem.available/1024/1024) + '\n')
+	print("CPU: ", cpu)
+	#time.sleep(INTERVAL)
