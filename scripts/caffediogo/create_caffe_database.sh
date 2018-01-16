10d8c3266f2a0d969046aae7d6109778f38eab11
kevindehecker <kevindehecker@hotmail.com>
2018-01-16 01:39:19 +0100

    Small fixes and include labels

--- scripts/caffediogo/create_caffe_database.sh
+++ scripts/caffediogo/create_caffe_database.sh
@@ -1,3 +1,13 @@
+#!/usr/bin/env bash
+
+#cd /data/kevin/kitti/raw_data/2011_09_26
+#find /data/kevin/kitti/raw_data/2011_09_26/ -iname *.png | grep image_03 > /data/kevin/kitti/raw_data/2011_09_26/images3.txt
+#find /data/kevin/kitti/raw_data/2011_09_26/ -iname *.png | grep image_02 > /data/kevin/kitti/raw_data/2011_09_26/images2.txt
+#find /data/kevin/kitti/raw_data/2011_09_26/ -iname *.png | grep disp> /data/kevin/kitti/raw_data/2011_09_26/disp.txt
+#find /data/kevin/kitti/raw_data/2011_09_26/ -iname *.png | grep conf > /data/kevin/kitti/raw_data/2011_09_26/conf.txt
+
+
+#cd /data/kevin/kitti/setup
 
 #depth laser ground truth
 find /data/kevin/kitti/data_depth_annotated/train/2011_09_26_drive_0*/proj_depth/groundtruth/image_03/  -iname *.png | grep image_03 > /data/kevin/kitti/data_depth_annotated/train/train_laser_images3.txt
@@ -18,7 +28,7 @@ sed -i 's/data_depth_annotated\/val/raw_data\/2011_09_26/g' /data/kevin/kitti/ra
 sed -i 's/proj_depth\/groundtruth\/image_03/image_02\/data/g' /data/kevin/kitti/raw_data/2011_09_26/val_images2.txt
 
 sed -i 's/data_depth_annotated\/val/raw_data\/2011_09_26/g' /data/kevin/kitti/raw_data/2011_09_26/val_images3.txt
-sed -i 's/proj_depth\/groundtruth\/image_02/image_03\/data/g' /data/kevin/kitti/raw_data/2011_09_26/val_images3.txt
+sed -i 's/proj_depth\/groundtruth\/image_03/image_03\/data/g' /data/kevin/kitti/raw_data/2011_09_26/val_images3.txt
 
 sed -i 's/data_depth_annotated\/val/raw_data\/2011_09_26/g' /data/kevin/kitti/raw_data/2011_09_26/val_conf.txt
 sed -i 's/proj_depth\/groundtruth\/image_03/conf/g' /data/kevin/kitti/raw_data/2011_09_26/val_conf.txt
@@ -28,13 +38,12 @@ sed -i 's/data_depth_annotated\/val/raw_data\/2011_09_26/g' /data/kevin/kitti/ra
 sed -i 's/proj_depth\/groundtruth\/image_03/disp/g' /data/kevin/kitti/raw_data/2011_09_26/val_disp.txt
 sed -i 's/.png/_disparity.png/g' /data/kevin/kitti/raw_data/2011_09_26/val_disp.txt
 
-
 #search and replace the validation train paths 
 sed -i 's/data_depth_annotated\/train/raw_data\/2011_09_26/g' /data/kevin/kitti/raw_data/2011_09_26/train_images2.txt
 sed -i 's/proj_depth\/groundtruth\/image_03/image_02\/data/g' /data/kevin/kitti/raw_data/2011_09_26/train_images2.txt
 
 sed -i 's/data_depth_annotated\/train/raw_data\/2011_09_26/g' /data/kevin/kitti/raw_data/2011_09_26/train_images3.txt
-sed -i 's/proj_depth\/groundtruth\/image_02/image_03\/data/g' /data/kevin/kitti/raw_data/2011_09_26/train_images3.txt
+sed -i 's/proj_depth\/groundtruth\/image_03/image_03\/data/g' /data/kevin/kitti/raw_data/2011_09_26/train_images3.txt
 
 sed -i 's/data_depth_annotated\/train/raw_data\/2011_09_26/g' /data/kevin/kitti/raw_data/2011_09_26/train_conf.txt
 sed -i 's/proj_depth\/groundtruth\/image_03/conf/g' /data/kevin/kitti/raw_data/2011_09_26/train_conf.txt
@@ -43,3 +52,14 @@ sed -i 's/.png/_confidence.png/g' /data/kevin/kitti/raw_data/2011_09_26/train_co
 sed -i 's/data_depth_annotated\/train/raw_data\/2011_09_26/g' /data/kevin/kitti/raw_data/2011_09_26/train_disp.txt
 sed -i 's/proj_depth\/groundtruth\/image_03/disp/g' /data/kevin/kitti/raw_data/2011_09_26/train_disp.txt
 sed -i 's/.png/_disparity.png/g' /data/kevin/kitti/raw_data/2011_09_26/train_disp.txt
+
+#add the silent label:
+awk '{printf("%s %d\n",$0,0)}' /data/kevin/kitti/raw_data/2011_09_26/val_images3.txt > /data/kevin/kitti/raw_data/2011_09_26/val_images3_labeled.txt
+awk '{printf("%s %d\n",$0,0)}' /data/kevin/kitti/raw_data/2011_09_26/val_images2.txt > /data/kevin/kitti/raw_data/2011_09_26/val_images2_labeled.txt
+awk '{printf("%s %d\n",$0,0)}' /data/kevin/kitti/raw_data/2011_09_26/val_disp.txt > /data/kevin/kitti/raw_data/2011_09_26/val_disp_labeled.txt
+awk '{printf("%s %d\n",$0,0)}' /data/kevin/kitti/raw_data/2011_09_26/val_conf.txt > /data/kevin/kitti/raw_data/2011_09_26/val_conf_labeled.txt
+
+awk '{printf("%s %d\n",$0,0)}' /data/kevin/kitti/raw_data/2011_09_26/train_images3.txt > /data/kevin/kitti/raw_data/2011_09_26/train_images3_labeled.txt
+awk '{printf("%s %d\n",$0,0)}' /data/kevin/kitti/raw_data/2011_09_26/train_images2.txt > /data/kevin/kitti/raw_data/2011_09_26/train_images2_labeled.txt
+awk '{printf("%s %d\n",$0,0)}' /data/kevin/kitti/raw_data/2011_09_26/train_disp.txt > /data/kevin/kitti/raw_data/2011_09_26/train_disp_labeled.txt
+awk '{printf("%s %d\n",$0,0)}' /data/kevin/kitti/raw_data/2011_09_26/train_conf.txt > /data/kevin/kitti/raw_data/2011_09_26/train_conf_labeled.txt
