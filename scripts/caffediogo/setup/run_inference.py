fb6d9eccee018d82f415e1c378b074298a26ae6d
Your Name <you@example.com>
2018-01-21 00:23:42 +0100

    Trivial changes

--- scripts/caffediogo/setup/run_inference.py
+++ scripts/caffediogo/setup/run_inference.py
@@ -8,12 +8,13 @@ import cv2
 import matplotlib.pyplot as plt
 from PIL import Image
 
-caffe.set_mode_gpu()
-caffe.set_device(1)
+caffe.set_mode_cpu()
+#caffe.set_device(0)
 
-net = caffe.Net("/data/kevin/kitti/setup/net_runSparse10.prototxt", "/data/kevin/kitti/cheapSparse10/run_iter_50000.caffemodel",0)
+net = caffe.Net("/data/kevin/kitti/setup/net_runSparse10.prototxt", "/data/kevin/kitti/cheapSparse10_2/_iter_35000.caffemodel",0)
 
-im = Image.open("/data/kevin/kitti/raw_data/2011_09_26/2011_09_26_drive_0001_sync/image_03/data/0000000000.png")
+input_file = sys.argv[1]
+im = Image.open(input_file)
 im = im.resize((298, 218), Image.NEAREST)
 nim = np.array(im)
 nim = nim.reshape(1,3,218,298)
@@ -24,6 +25,7 @@ nim = nim.reshape(1,3,218,298)
 net.blobs['inputData'].data[...] = nim
 out = net.forward()
 mat = out['fine_depthReLU'][0]
+mat = mat * 255
 print(mat,mat.shape)
 mat = (mat[0,:,:]).astype('uint8')
 cv2.imwrite('test.png',mat)
