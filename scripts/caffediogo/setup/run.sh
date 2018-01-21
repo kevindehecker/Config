55de8f2eb53b3afc6637727ebf3e07dfe043ffa8
Your Name <you@example.com>
2018-01-21 17:56:25 +0100

    Added logging and automated script

--- scripts/caffediogo/setup/run.sh
+++ scripts/caffediogo/setup/run.sh
@@ -1,1 +1,9 @@
-/home/kevin/caffe/build/tools/caffe train -solver solverSparse10.prototxt -weights model_norm_abs_100k.caffemodel -weights model_norm_abs_100k.caffemodel -gpu 1
+set -ex
+snapdir=/data/kevin/kitti/cheapSparse10_3/
+
+mkdir -p $snapdir
+
+cp solver.prototxt.template solver_autoconfigure.prototxt
+echo snapshot_prefix: \"${snapdir}run\" >> solver_autoconfigure.prototxt
+
+/home/kevin/caffe/build/tools/caffe train -solver solver_autoconfigure.prototxt -weights model_norm_abs_100k.caffemodel -gpu 1 2>&1 | tee $snapdir/logfile.log
