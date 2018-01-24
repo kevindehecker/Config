1961a076354ab3c2cbb37ac04cb8b76e928d847c
Your Name <you@example.com>
2018-01-24 22:50:51 +0100

    Train from scratch

--- scripts/caffediogo/setup/run.sh
+++ scripts/caffediogo/setup/run.sh
@@ -1,9 +1,9 @@
 set -ex
-snapdir=/data/kevin/kitti/cheapSparse10_3/
+snapdir=/data/kevin/kitti/cheapSparse10/
 
 mkdir -p $snapdir
 
 cp solver.prototxt.template solver_autoconfigure.prototxt
 echo snapshot_prefix: \"${snapdir}run\" >> solver_autoconfigure.prototxt
 
-/home/kevin/caffe/build/tools/caffe train -solver solver_autoconfigure.prototxt -weights model_norm_abs_100k.caffemodel -gpu 1 2>&1 | tee $snapdir/logfile.log
+/home/kevin/caffe/build/tools/caffe train -solver solver_autoconfigure.prototxt -gpu 1 2>&1 | tee $snapdir/logfile.log
