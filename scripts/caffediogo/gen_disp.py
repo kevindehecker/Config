a4e73f88d115d41dcb5f27a9c9c30224af6055e9
guidoAI <guido.de.croon@gmail.com>
2018-01-24 21:03:58 +0100

    now a left-right check and no prefilter cap, less penalty for non-smoothness

--- scripts/caffediogo/gen_disp.py
+++ scripts/caffediogo/gen_disp.py
@@ -34,6 +34,10 @@ def show_disparity_maps():
         write_images(imgL, imgR, disp, min_disp, num_disp, thresh1);
     elif(image_set == 'KITTI'):
 
+        SHOW = False;
+        WRITE = True;
+        im_step = 1;
+
         fname_left = "images2.txt"; #"/data/kevin/kitti/raw_data/2011_09_26/images3.txt";
         fname_right = "images3.txt";
         with open(fname_left) as f:
@@ -47,11 +51,13 @@ def show_disparity_maps():
         images_right = [x.strip() for x in images_right] 
 
         # iterate over the files:
-        for idx, im in enumerate(images_left):
+        # for idx, im in enumerate(images_left):
+        for idx in np.arange(0, len(images_left), im_step):
 
             print("{} / {}".format(idx, len(images_left)));
 
             # read the image (consisting of a right image and left image)
+            im = images_left[idx];
             imgL = cv2.imread(im);
             imgR = cv2.imread(images_right[idx]);
 
@@ -63,8 +69,9 @@ def show_disparity_maps():
             sobelX = cv2.Sobel(grey,cv2.CV_64F,1,0,ksize=5);
             ret,thresh1 = cv2.threshold(sobelX,70,255,cv2.THRESH_BINARY);
             
-            # show the output
-            show_disparity_map(imgL, imgR, disp, min_disp, num_disp, thresh1);
+            if(SHOW):
+              # show the output
+              show_disparity_map(imgL, imgR, disp, min_disp, num_disp, thresh1);
 
             base_name = os.path.basename(im);
             file_name, ext = os.path.splitext(base_name);
@@ -73,7 +80,9 @@ def show_disparity_maps():
             dir_name = os.path.dirname(dir_name);
             dir_name = dir_name;
 
-            write_images(dir_name, file_name, imgL, imgR, disp, min_disp, num_disp, thresh1);
+            if(WRITE):
+              # write the output:
+              write_images(dir_name, file_name, imgL, imgR, disp, min_disp, num_disp, thresh1);
         
 
     else:
@@ -122,8 +131,22 @@ def write_images(dir_name, file_name, imgL, imgR, disp, min_disp, num_disp, conf
 
 def calculate_disparities(imgL, imgR, window_size, min_disp, num_disp):
 
-    # semi-global matching:
-    stereo = cv2.StereoSGBM_create(minDisparity=min_disp, numDisparities = num_disp, blockSize = window_size+1, preFilterCap=10, P1= 4 * 3 * window_size ** 2, P2 = 32 * 3 * window_size ** 2);
+    # semi-global matching: https://docs.opencv.org/trunk/d2/d85/classcv_1_1StereoSGBM.html
+
+    # minDisparity	Minimum possible disparity value. Normally, it is zero but sometimes rectification algorithms can shift images, so this parameter needs to be adjusted accordingly.
+    # numDisparities	Maximum disparity minus minimum disparity. The value is always greater than zero. In the current implementation, this parameter must be divisible by 16.
+    # blockSize	Matched block size. It must be an odd number >=1 . Normally, it should be somewhere in the 3..11 range.
+    # P1	The first parameter controlling the disparity smoothness. See below.
+    # P2	The second parameter controlling the disparity smoothness. The larger the values are, the smoother the disparity is. P1 is the penalty on the disparity change by plus or minus 1 between neighbor pixels. P2 is the penalty on the disparity change by more than 1 between neighbor pixels. The algorithm requires P2 > P1 . See stereo_match.cpp sample where some reasonably good P1 and P2 values are shown (like 8*number_of_image_channels*SADWindowSize*SADWindowSize and 32*number_of_image_channels*SADWindowSize*SADWindowSize , respectively).
+    # disp12MaxDiff	Maximum allowed difference (in integer pixel units) in the left-right disparity check. Set it to a non-positive value to disable the check.
+    # preFilterCap	Truncation value for the prefiltered image pixels. The algorithm first computes x-derivative at each pixel and clips its value by [-preFilterCap, preFilterCap] interval. The result values are passed to the Birchfield-Tomasi pixel cost function.
+    # uniquenessRatio	Margin in percentage by which the best (minimum) computed cost function value should "win" the second best value to consider the found match correct. Normally, a value within the 5-15 range is good enough.
+    # speckleWindowSize	Maximum size of smooth disparity regions to consider their noise speckles and invalidate. Set it to 0 to disable speckle filtering. Otherwise, set it somewhere in the 50-200 range.
+    # speckleRange	Maximum disparity variation within each connected component. If you do speckle filtering, set the parameter to a positive value, it will be implicitly multiplied by 16. Normally, 1 or 2 is good enough.
+    # mode	Set it to StereoSGBM::MODE_HH to run the full-scale two-pass dynamic programming algorithm. It will consume O(W*H*numDisparities) bytes, which is large for 640x480 stereo and huge for HD-size pictures. By default, it is set to false .
+
+    # stereo = cv2.StereoSGBM_create(minDisparity=min_disp, numDisparities = num_disp, blockSize = window_size+1, preFilterCap=10, P1= 4 * 3 * window_size ** 2, P2 = 32 * 3 * window_size ** 2);
+    stereo = cv2.StereoSGBM_create(minDisparity=min_disp, numDisparities = num_disp, blockSize = window_size, preFilterCap=0, P1= 4 * 3 * window_size ** 2, P2 = 16 * 3 * window_size ** 2, disp12MaxDiff = 2);
     disp = stereo.compute(imgL, imgR).astype(np.float32) / 16.0;
     return disp; 
 
