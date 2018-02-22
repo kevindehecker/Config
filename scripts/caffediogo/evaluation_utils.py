corrected the calculation of error

scripts/caffediogo/evaluation_utils.py
@@ -13,19 +13,30 @@ import os
 import cv2
 from collections import Counter
 import pdb
+import matplotlib.pyplot as plt
 
-def compute_errors(gt, pred):
+def compute_errors(gt, pred, graphics = False, name_fig='error map'):
+
+    if(graphics):
+        Mask = gt == 0;
+        Mask = 1.0 - Mask;
+        AbsErr = np.abs(gt - pred);
+        AbsErr = np.multiply(Mask, AbsErr);
+        plt.figure();
+        plt.imshow(AbsErr);
+        plt.colorbar();
+        plt.title(name_fig);
+        
     
-    inds1 = gt > 0;
-    inds2 = pred > 0;
-    inds = np.logical_and(inds1, inds2);
+#    inds1 = gt > 0;
+#    inds2 = pred > 0;
+#    inds = np.logical_and(inds1, inds2);
+    inds = gt > 0;
     gt = gt[inds];
     pred = pred[inds];
-    
-    thresh = np.maximum((gt / pred), (pred / gt))
-    a1 = (thresh < 1.25   ).mean()
-    a2 = (thresh < 1.25 ** 2).mean()
-    a3 = (thresh < 1.25 ** 3).mean()
+
+    inds = pred == 0;
+    pred[inds] += 1E-4;
 
     rmse = (gt - pred) ** 2
     rmse = np.sqrt(rmse.mean())
@@ -36,6 +47,11 @@ def compute_errors(gt, pred):
     abs_rel = np.mean(np.abs(gt - pred) / gt)
 
     sq_rel = np.mean(((gt - pred)**2) / gt)
+
+    thresh = np.maximum((gt / pred), (pred / gt))
+    a1 = (thresh < 1.25   ).mean()
+    a2 = (thresh < 1.25 ** 2).mean()
+    a3 = (thresh < 1.25 ** 3).mean()
 
     return abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3
 

scripts/caffediogo/merge.py
@@ -76,16 +76,18 @@ def merge_Diogo(stereo_map, mono_map, image, graphics = False):
         axes[1].set_title('Stereo');
         axes[2].imshow(fusion);
         axes[2].set_title('Fusion');
-        
         fig, axes = plt.subplots(nrows=1, ncols=2)
         axes[0].imshow(image);
         axes[0].set_title('Image');
         axes[1].imshow(stereo_confidence);    
+        axes[1].set_title('Stereo confidence');        
     
     return fusion;
     
     
 def determine_stereo_confidence(stereo_map, image, blur_window = 3, gradient_threshold = 175, graphics = False):
+
+    # TODO: use matching cost / uncertainty of stereo
 
     # always when there is no stereo output, low conf
     ret,thresh0 = cv2.threshold(stereo_map, 0, 255, cv2.THRESH_BINARY); 
@@ -96,7 +98,7 @@ def determine_stereo_confidence(stereo_map, image, blur_window = 3, gradient_thr
     if(graphics):
         plt.figure()
         plt.imshow(blur);
-        plt.title('initial blur stereo')
+        plt.title('initial blur grayscale for conf stereo')
     
     #blur = grey;
     sobelX = cv2.Sobel(blur,cv2.CV_64F,1,0,ksize=5);
@@ -105,7 +107,7 @@ def determine_stereo_confidence(stereo_map, image, blur_window = 3, gradient_thr
     if(graphics):
         plt.figure();
         plt.imshow(thresh1);
-        plt.title('thresh1');
+        plt.title('thresh1: enough vertical contrast');
     
     # combine the confidence of thresh 0 (invalid matches) and thresh 1 (edges)
     thresh1 = cv2.bitwise_and(thresh0.astype(np.uint8),thresh1.astype(np.uint8));  
@@ -114,17 +116,17 @@ def determine_stereo_confidence(stereo_map, image, blur_window = 3, gradient_thr
     if(graphics):
         plt.figure()
         plt.imshow(thresh1);
-        plt.title('thresh2')    
+        plt.title('thresh2: vertical contrast and valid match')    
     
     
     # blur the threshold image
     blur = cv2.GaussianBlur(thresh1,(blur_window*2+1,blur_window*2+1),0);
     blur /= np.max(blur[:]);
     
-    if(graphics):
-        plt.figure()
-        plt.imshow(blur);
-        plt.title('second blur stereo')    
+#    if(graphics):
+#        plt.figure()
+#        plt.imshow(blur);
+#        plt.title('second blur stereo')    
     
     # on the edges, the confidence should be 1
     stereo_confidence = blur;
@@ -140,6 +142,7 @@ def determine_stereo_confidence(stereo_map, image, blur_window = 3, gradient_thr
 def scale_mono_map(stereo_map, mono_map):
     
     # Diogo's original method (TODO: make more robust to outliers):
+    # TODO: what is the best scaling for performance?
     max_stereo = np.max(stereo_map[stereo_map != 0]);
     min_stereo = np.min(stereo_map[stereo_map != 0]);
     min_mono = np.min(mono_map[:]);

scripts/caffediogo/performance.py
@@ -37,7 +37,7 @@ def merge_depth_maps(mono_name = "/home/guido/cnn_depth_tensorflow/tmp/00002.png
     mono = cv2.imread(mono_name);
     mono = cv2.cvtColor(mono, cv2.COLOR_RGB2GRAY);
     mono = mono.astype(float);
-    mono /= 255.0 / MAX_DISP;
+    mono /= 255.0 / MAX_DISP; # TODO: is this correct for mancini?
     # mono = cv2.resize(mono, (64, 20), interpolation=cv2.INTER_NEAREST);
 
     stereo = cv2.imread(stereo_name);
@@ -84,7 +84,8 @@ def merge_depth_maps(mono_name = "/home/guido/cnn_depth_tensorflow/tmp/00002.png
     
     depth_stereo = evaluation_utils.convert_disps_to_depths_kitti(stereo);
     depth_mono = evaluation_utils.convert_disps_to_depths_kitti(mono);
-    depth_GT = evaluation_utils.convert_disps_to_depths_kitti(GT);
+    depth_GT = GT;
+    # depth_GT = evaluation_utils.convert_disps_to_depths_kitti(GT);
     depth_fusion = merge.merge_Diogo(depth_stereo, depth_mono, image, graphics = False);
     
     if(graphics):
@@ -103,21 +104,31 @@ def merge_depth_maps(mono_name = "/home/guido/cnn_depth_tensorflow/tmp/00002.png
         fig.colorbar(cf, ax=axes[1,1])
     
     performance = np.zeros([3, 7]);
-    abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3 = evaluation_utils.compute_errors(depth_GT[:], depth_stereo[:]);
+    abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3 = evaluation_utils.compute_errors(depth_GT[:], depth_stereo[:], name_fig = 'stereo error map');
     performance[0,:] = [abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3];
-    abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3 = evaluation_utils.compute_errors(depth_GT[:], depth_mono[:]);
+    abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3 = evaluation_utils.compute_errors(depth_GT[:], depth_mono[:], name_fig = 'mono error map');
     performance[1,:] = [abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3];
-    abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3 = evaluation_utils.compute_errors(depth_GT[:], depth_fusion[:]);
+    abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3 = evaluation_utils.compute_errors(depth_GT[:], depth_fusion[:], name_fig = 'fusion error map');
     performance[2,:] = [abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3];
     
     if(verbose):
         print_performance(performance);
-    
+
+    # scaled mono
+#    depth_mono = merge.scale_mono_map(depth_stereo, depth_mono);    
+#    abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3 = evaluation_utils.compute_errors(depth_GT[:], depth_mono[:]);
+#    performance[1,:] = [abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3];    
+#    
+#    if(verbose):
+#        print('Scaled mono:')
+#        print_performance(performance);
+#        
+        
     return performance, depth_fusion;
 
 # merge_depth_maps(graphics=True);
-merge_depth_maps(mono_name = "./tmp/0000000061_sperziboon.png", 
-                     stereo_name = "./tmp/0000000061_disparity.png",
-                     GT_name = "./tmp/0000000061_GT.png",
-                     image_name = "./tmp/0000000061_image.png",
-                     graphics = True, verbose = True)
+#merge_depth_maps(mono_name = "./tmp/0000000013_sperziboon.png", 
+#                     stereo_name = "./tmp/0000000013_disparity.png",
+#                     GT_name = "./tmp/0000000013_GT.png",
+#                     image_name = "./tmp/0000000013_image.png",
+#                     graphics = False, verbose = True)