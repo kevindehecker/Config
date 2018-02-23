corrected scaling mistake mancini and introduced option non occluded

scripts/caffediogo/evaluation_utils.py
@@ -15,7 +15,7 @@ from collections import Counter
 import pdb
 import matplotlib.pyplot as plt
 
-def compute_errors(gt, pred, graphics = False, name_fig='error map'):
+def compute_errors(gt, pred, graphics = True, name_fig='error map', non_occluded=True):
 
     if(graphics):
         Mask = gt == 0;
@@ -28,30 +28,34 @@ def compute_errors(gt, pred, graphics = False, name_fig='error map'):
         plt.title(name_fig);
         
     
-#    inds1 = gt > 0;
-#    inds2 = pred > 0;
-#    inds = np.logical_and(inds1, inds2);
-    inds = gt > 0;
-    gt = gt[inds];
-    pred = pred[inds];
-
-    inds = pred == 0;
-    pred[inds] += 1E-4;
+    if(non_occluded):
+        inds1 = gt > 0;
+        inds2 = pred > 0;
+        inds = np.logical_and(inds1, inds2);
+        gt = gt[inds];
+        pred = pred[inds];
+    else:
+        inds = gt > 0;
+        gt = gt[inds];
+        pred = pred[inds];
+        inds = pred == 0;
+        pred[inds] += 1E-4;
 
     rmse = (gt - pred) ** 2
     rmse = np.sqrt(rmse.mean())
-
+    
+    # TODO: should this be log10?
     rmse_log = (np.log(gt) - np.log(pred)) ** 2
     rmse_log = np.sqrt(rmse_log.mean())
-
+    
     abs_rel = np.mean(np.abs(gt - pred) / gt)
 
     sq_rel = np.mean(((gt - pred)**2) / gt)
 
-    thresh = np.maximum((gt / pred), (pred / gt))
-    a1 = (thresh < 1.25   ).mean()
-    a2 = (thresh < 1.25 ** 2).mean()
-    a3 = (thresh < 1.25 ** 3).mean()
+    delta = np.maximum((gt / pred), (pred / gt))
+    a1 = (delta < 1.25   ).mean()
+    a2 = (delta < 1.25 ** 2).mean()
+    a3 = (delta < 1.25 ** 3).mean()
 
     return abs_rel, sq_rel, rmse, rmse_log, a1, a2, a3
 