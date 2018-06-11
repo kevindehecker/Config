53d768b4fa9aa0acd6bc777d1069e14523a911b1
kevindehecker <kevindehecker@hotmail.com>
2018-06-11 16:06:05 +0200

    Update odroid_install_realsense.sh
    screen install

--- scripts/odroid_install_realsense.sh
+++ scripts/odroid_install_realsense.sh
@@ -20,7 +20,7 @@ sudo reboot
 #manually upgrade packages that were left out for various reasons
 sudo apt-get install libdrm-amdgpu1 libdrm-dev libdrm-etnaviv1 libdrm-exynos1 libdrm-freedreno1 libdrm-nouveau2 libdrm-omap1 libdrm-radeon1 libdrm-tegra0 libdrm2 libegl1-mesa libegl1-mesa-dev libgbm1 libgl1-mesa-dri libmm-glib0 libqmi-proxy libwayland-egl1-mesa linux-headers-xu3 linux-image-xu3 mesa-vdpau-drivers modemmanager -y
 
-sudo apt-get install git cmake git libssl-dev libusb-1.0-0-dev pkg-config libgtk-3-dev openssh-server libglfw3-dev gedit -y
+sudo apt-get install git cmake git libssl-dev libusb-1.0-0-dev pkg-config libgtk-3-dev openssh-server libglfw3-dev gedit screen -y
 sudo apt-get install git-core bash-completion
 sudo apt-get install gstreamer1.0-tools gstreamer1.0-alsa   gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-libav -y libgstreamer-plugins-base1.0-* libgstreamer-plugins-bad1.0-* libgstreamer-plugins-good1.0-*
 
