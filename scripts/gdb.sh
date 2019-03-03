762d1661e16c5827591e974e3da5d57793b0fa9a
kevindehecker <kevindehecker@hotmail.com>
2019-03-03 11:44:39 +0100

    Backup some stuff

---
+++ scripts/gdb.sh
@@ -0,0 +1,4 @@
+target extended-remote /dev/ttyACM0
+mon swdp_scan
+att 1
+load
--- scripts/mount_share.sh
+++ scripts/mount_share.sh
@@ -4,3 +4,8 @@ id -u ubuntu
 sudo mount -t cifs //192.168.1.11/jetserketser -o username=houjebek,password=<PASSWORD>,uid=1000,gid=1000 hv
 
 sudo mount -t cifs -o vers=1.0 //192.168.1.11/jetserketser -o username=houjebek,password=pass,uid=1000,gid=1000 hv
+
+sudo mount -t cifs //192.168.1.11/jetserketser -o username=houjebek,password=<PASSWORD>,uid=1000,gid=1000 ~/shares/jetserketser/
+sudo mount -t cifs //192.168.1.13/terantisch -o username=houjebek,password=<PASSWORD>,uid=1000,gid=1000 ~/shares/terantisch/
+sudo mount -t cifs //192.168.1.13/lalaladata -o username=houjebek,password=<PASSWORD>,uid=1000,gid=1000 ~/shares/lalaladata/
+sudo mount -t cifs //192.168.1.13/JoeHoe -o username=houjebek,password=<PASSWORD>,uid=1000,gid=1000 ~/shares/joehoe/
