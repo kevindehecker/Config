6877ee0eaba048f82f9dba7d69cdfec07f1c7f66
Kevin <kevindehecker@hotmail.com>
2021-01-16 20:35:24 +0100

    synckee

---
+++ scripts/synckee.sh
@@ -0,0 +1,4 @@
+rsync -tuv bigpats_ext:../../mnt/HD/HD_a2/kevin/kevin.kdbx /home/houjebek/Bla/Cloud/kevin.kdbx
+rsync -tuv /home/houjebek/Bla/Cloud/kevin.kdbx bigpats_ext:../../mnt/HD/HD_a2/kevin/kevin.kdbx
+
+
