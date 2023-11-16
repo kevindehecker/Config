a7031463c0744b3c8979bfe9e4dbe1328865c791
kevindehecker <kevindehecker@hotmail.com>
2023-11-16 11:13:48 +0100

    Update add_new_ssh_key.sh

--- scripts/add_new_ssh_key.sh
+++ scripts/add_new_ssh_key.sh
@@ -1,4 +1,4 @@
-ssh-keygen -t rsa -b 4096 -C "kevindehecker@hotmail.com"
+ssh-keygen -t ed25519 -C "kevindehecker@hotmail.com"
 eval "$(ssh-agent -s)" #check if process exists
 ssh-add ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub
