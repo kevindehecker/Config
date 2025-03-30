506093b63f5475cd5e27e089c705f05478433f82
kevindehecker <kevindehecker@hotmail.com>
2025-03-30 10:47:17 +0200

    Update mount_share.sh

--- scripts/mount_share.sh
+++ scripts/mount_share.sh
@@ -1,11 +1,30 @@
 id -g ubuntu
 id -u ubuntu
 
-sudo mount -t cifs //192.168.1.11/jetserketser -o username=houjebek,password=<PASSWORD>,uid=1000,gid=1000 hv
+sudo mkdir -p /etc/smbcredentials
+sudo nano /etc/smbcredentials/houjebek
 
-sudo mount -t cifs -o vers=1.0 //192.168.1.11/jetserketser -o username=houjebek,password=pass,uid=1000,gid=1000 hv
+# username=<USER>
+# password=<PASS>
 
-sudo mount -t cifs //192.168.1.11/jetserketser -o username=houjebek,password=<PASSWORD>,uid=1000,gid=1000 ~/shares/jetserketser/
-sudo mount -t cifs //192.168.1.13/terantisch -o username=houjebek,password=<PASSWORD>,uid=1000,gid=1000 ~/shares/terantisch/
-sudo mount -t cifs //192.168.1.13/lalaladata -o username=houjebek,password=<PASSWORD>,uid=1000,gid=1000 ~/shares/lalaladata/
-sudo mount -t cifs //192.168.1.13/JoeHoe -o username=houjebek,password=<PASSWORD>,uid=1000,gid=1000 ~/shares/joehoe/
+sudo chmod 600 /etc/smbcredentials/houjebek
+
+
+sudo mount -t cifs //192.168.68.54/Film /home/houjebek/shares/Film -o credentials=/etc/smbcredentials/houjebek,uid=1000,gid=1000,rw,vers=3.0
+sudo mount -t cifs //192.168.68.54/Serie /home/houjebek/shares/Serie -o credentials=/etc/smbcredentials/houjebek,uid=1000,gid=1000,rw,vers=3.0
+sudo mount -t cifs //192.168.68.54/Backup /home/houjebek/shares/Backup -o credentials=/etc/smbcredentials/houjebek,uid=1000,gid=1000,rw,vers=3.0
+
+# or put in /etc/fstab:
+# //192.168.68.54/Film /home/houjebek/shares/Film cifs credentials=/etc/smbcredentials/houjebek,uid=1000,gid=1000,vers=3.0 0 0
+# ...# 
+
+# similar for pats:
+sudo mkdir -p /etc/smbcredentials
+sudo nano /etc/smbcredentials/kevin
+sudo chmod 600 /etc/smbcredentials/kevin
+
+
+sudo mount -t cifs //192.168.68.54/Film /home/houjebek/shares/Monitoring -o credentials=/etc/smbcredentials/kevin,uid=1000,gid=1000,rw,vers=3.0
+sudo mount -t cifs //192.168.68.54/BagLogs /home/houjebek/shares/BagLogs -o credentials=/etc/smbcredentials/kevin,uid=1000,gid=1000,rw,vers=3.0
+sudo mount -t cifs //192.168.68.54/Backup /home/houjebek/shares/Backup -o credentials=/etc/smbcredentials/kevin,uid=1000,gid=1000,rw,vers=3.0
+
